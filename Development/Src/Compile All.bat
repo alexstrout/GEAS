9c2367daf547fb05ae9dd6ce3a7c375914ba8df2
Alex Strout <alex@taraxis.com>
2013-11-18 18:40:03 -0500

    Basic SimTest stuff

--- Development/Src/Compile All.bat
+++ Development/Src/Compile All.bat
@@ -1,5 +1,5 @@
 @Echo off
 echo Starting Compile Job...
-C:\UDK\UDK-2012-05\Binaries\Win32\UDK make -full -intermediate LOG=Compile.log
+C:\UDK\UDK-2013-07\Binaries\Win32\UDK make -full -intermediate LOG=Compile.log
 echo.
 pause
